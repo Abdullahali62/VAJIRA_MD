5d801cb2a263126a17b5f5534f236836cb2011a9
Abdullahali62 <salefor472@gmail.com>
2024-12-06 23:03:54 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,10 +7,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711453361' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923044586509' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
-PREFIX: process.env.PREFIX || '.' ,
-FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+PREFIX: process.env.PREFIX || '@' ,
+FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 Hassan 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 PAIR: 'https://vajiraqrveb-1d064bcd8502.herokuapp.com/code?number=',       
